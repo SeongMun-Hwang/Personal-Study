8545f8a9b66ff1e643e1d16d12555d16a630e008
SeongMun-Hwang <hwang105814@gmail.com>
2023-11-16 16:45:14 +0900

    2063

---
+++ swacademy/1/2063.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> number(n);
+    for(int i=0;i<n;i++){
+        cin >> number[i];
+    }
+    sort(number.begin(),number.end());
+    cout << number[n/2];
+}
