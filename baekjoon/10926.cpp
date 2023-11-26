232005b098f7a565c2e65b4dfa8d6fe81e6dd012
seongmun-hwang <hwang105814@gmail.com>
2023-11-26 22:49:46 +0900

    10926

---
+++ baekjoon/10926.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	string s;
+	cin >> s;
+	cout << s + "??!";
+	return 0;
+}
