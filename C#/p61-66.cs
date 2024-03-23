c3f0dae25a27e98228b8b9b1cd6a58a5295b8a83
seongmun-hwang <hwang105814@gmail.com>
2024-03-23 21:20:07 +0900

    p61-66

---
+++ C#/p61-66.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace CsConsole
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            char a = 'h';
+            char b = 'e';
+            char c = 'l';
+            char d = 'l';
+            char e = 'o';
+            string f = "world!";
+            string g = "My name is\nSeongmun Hwang";
+            object h = "Nice to meet you";
+
+            Console.Write(a);
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.Write(h);
+        }
+    }
+}
