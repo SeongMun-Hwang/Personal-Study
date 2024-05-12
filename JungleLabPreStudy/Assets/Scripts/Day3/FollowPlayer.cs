1096e6c8207a500008733c1bb0f32b1b30deb7bf
seongmun-hwang <hwang105814@gmail.com>
2024-05-12 22:58:32 +0900

    Day 3 - Add Camera following using LateUpdate

--- JungleLabPreStudy/Assets/Scripts/Day3/FollowPlayer.cs
+++ JungleLabPreStudy/Assets/Scripts/Day3/FollowPlayer.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 8, -7);
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.Translate(Vector3.forward*Time.deltaTime*20);
+        transform.position = player.transform.position + offset;
     }
 }
