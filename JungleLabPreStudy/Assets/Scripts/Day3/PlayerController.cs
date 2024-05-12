fbe66259f20dcd9a62e8a45c67da153c612658cb
seongmun-hwang <hwang105814@gmail.com>
2024-05-12 23:13:07 +0900

    Day 3 - Add car control

--- JungleLabPreStudy/Assets/Scripts/Day3/PlayerController.cs
+++ JungleLabPreStudy/Assets/Scripts/Day3/PlayerController.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime* turnSpeed*horizontalInput);
     }
 }
