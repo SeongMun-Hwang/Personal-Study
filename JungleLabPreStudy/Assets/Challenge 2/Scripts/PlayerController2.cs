427c8a9324e2587f5f8da1bf62c4ed19e9e921e5
seongmun-hwang <hwang105814@gmail.com>
2024-05-15 17:33:34 +0900

    Day 6 - Add dog send cooltime & Challenge complete

---
+++ JungleLabPreStudy/Assets/Challenge 2/Scripts/PlayerController2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float coolTime = 1.0f;
+    private float pressTime = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&&(Time.time-pressTime)>coolTime)
+        {
+            pressTime = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
