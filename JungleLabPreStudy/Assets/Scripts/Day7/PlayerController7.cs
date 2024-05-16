1ef3c96c06c622219bb502f1115e4a6d74d3a58f
seongmun-hwang <hwang105814@gmail.com>
2024-05-16 20:01:58 +0900

    Day7- Chapter3-3 Complete

--- JungleLabPreStudy/Assets/Scripts/Day7/PlayerController7.cs
+++ JungleLabPreStudy/Assets/Scripts/Day7/PlayerController7.cs
@@ -14,23 +14,27 @@ public class PlayerController7 : MonoBehaviour
     public float playerSpeed = 20;
     //gameover
     public bool gameOver = false;
-    
+    //animation
+    private Animator playerAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         xInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.forward * xInput*Time.deltaTime* playerSpeed);
-        if (Input.GetKeyDown(KeyCode.Space)&&isOnGround)
+        transform.Translate(Vector3.forward * xInput * Time.deltaTime * playerSpeed);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnimator.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -41,6 +45,8 @@ public class PlayerController7 : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", Random.Range(1, 3));
             gameOver = true;
             Debug.Log("Game Over!");
         }
