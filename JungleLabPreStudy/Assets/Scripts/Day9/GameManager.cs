228f1321cbd77b5644bc0eecc7b2e87b6b8cce07
seongmun-hwang <hwang105814@gmail.com>
2024-05-18 16:30:22 +0900

    Day9 - Chapter5-2 Complete

--- JungleLabPreStudy/Assets/Scripts/Day9/GameManager.cs
+++ JungleLabPreStudy/Assets/Scripts/Day9/GameManager.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreTesxt;
+    public int score;
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 
     IEnumerator SpawnTarget()
@@ -19,5 +23,10 @@ public class GameManager : MonoBehaviour
             int index=Random.Range(0,targets.Count);
             Instantiate(targets[index]);
         }
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score+= scoreToAdd;
+        scoreTesxt.text = "Score : " + score;
     }
 }
