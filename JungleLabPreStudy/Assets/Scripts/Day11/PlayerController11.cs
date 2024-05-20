6834aeba18b37196fc4a50834bc34dedb201ab78
seongmun-hwang <hwang105814@gmail.com>
2024-05-20 16:27:09 +0900

    Day 11 - Chapter 4-3 Complete

--- JungleLabPreStudy/Assets/Scripts/Day11/PlayerController11.cs
+++ JungleLabPreStudy/Assets/Scripts/Day11/PlayerController11.cs
@@ -8,6 +8,9 @@ public class PlayerController11 : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerUp;
+    private float powerupStrencth = 15.0f;
+    public GameObject powerUpIndiciator;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -19,5 +22,36 @@ public class PlayerController11 : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerUpIndiciator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        if (hasPowerUp)
+        {
+            powerUpIndiciator.transform.Rotate(Vector3.up, Time.deltaTime * 300);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            powerUpIndiciator.gameObject.SetActive(true);
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpTimer());
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRb=collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRb.AddForce(awayFromPlayer*powerupStrencth,ForceMode.Impulse);
+        }
+    }
+    IEnumerator PowerUpTimer()
+    {
+        yield return new WaitForSeconds(7.0f);
+        powerUpIndiciator.gameObject.SetActive(false);
+        hasPowerUp = false;
     }
 }
