6aff0b376c75594d72874209cbab14f3be2c14d0
seongmun-hwang <hwang105814@gmail.com>
2024-05-21 18:27:37 +0900

    Day 12 - Chapter 4-4 Complete

--- JungleLabPreStudy/Assets/Scripts/Day11/SpawnManager11.cs
+++ JungleLabPreStudy/Assets/Scripts/Day11/SpawnManager11.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class SpawnManager11 : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
     void Start()
     {
-        
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if(enemyCount == 0) {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
     private Vector3 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
+    }
+    void SpawnEnemyWave(int numberOfEnemy)
+    {
+        for(int i = 0; i < numberOfEnemy; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
