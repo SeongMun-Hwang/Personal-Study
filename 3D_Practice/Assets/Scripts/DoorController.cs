25a6aedeb5006cb364fb07ea16a078c0a597a871
seongmun-hwang <hwang105814@gmail.com>
2024-05-24 18:09:03 +0900

    FIx door control by localEulerAngles

--- 3D_Practice/Assets/Scripts/DoorController.cs
+++ 3D_Practice/Assets/Scripts/DoorController.cs
@@ -9,45 +9,51 @@ public class DoorController : MonoBehaviour
     public GameObject rotateAxis;
     
     private bool isOpening = false;
+    private bool isOpened = false;
 
     //Object name which approach to door
     private string objectName = "Player";
     private GameObject player;
-
+    private Vector3 initialRotate;
     void Start()
     {
         player = GameObject.Find(objectName);
+        initialRotate = transform.eulerAngles;
     }
     private void Update()
     {
-        float distance = Math.Abs(transform.position.x - player.transform.position.x);
+        float distance = Vector3.Distance(transform.position,player.transform.position);
         if (distance < 2 && !isOpening)
         {
+            Debug.Log("Opened");
             StartCoroutine(OpenDoor());
         }
-        else if (distance >= 2 && !isOpening)
+        else if (distance >= 2 && !isOpening && isOpened)
         {
+            Debug.Log("Closed");
             StartCoroutine(CloseDoor());
         }
     }
     IEnumerator OpenDoor()
     {
         isOpening = true;
-        while (rotateAxis.transform.eulerAngles.y < 90)
+        while (rotateAxis.transform.localEulerAngles.y < 90)
         {
             rotateAxis.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
             yield return null;
         }
         isOpening = false;
+        isOpened = true;
     }
     public IEnumerator CloseDoor()
     {
         isOpening = true;
-        while (rotateAxis.transform.eulerAngles.y > 1)
+        while (rotateAxis.transform.localEulerAngles.y > 1)
         {
             rotateAxis.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
             yield return null;
         }
         isOpening = false;
+        isOpened = false;
     }
 }
